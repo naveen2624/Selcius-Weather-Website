62200d01527d5709ba4552208c65f5e39c3c345a
naveen2624 <naveenudai26@gmail.com>
2025-03-10 01:41:32 +0530

    initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+const footer = () => {
+  return (
+    <div className="border-t background-blur py-12 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto px-4 text-center text-gray-400">
+        Made with ❤ by Naveen
+      </div>
+    </div>
+  );
+};
+
+export default footer;
